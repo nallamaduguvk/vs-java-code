843e2d0a168764f593cc4cd1b72e71795166eb39
nallamaduguvk <145623934+nallamaduguvk@users.noreply.github.com>
2024-02-06 10:42:40 -0600

    array

---
+++ array.java
@@ -0,0 +1,22 @@
+public class array {
+    public static void main(String[] args){
+        int nums[] = new int[8];
+        nums[0]= 5;
+        nums[1]= 6;
+        nums[2]= 55;
+        nums[3]= 48;
+        nums[4]= 54;
+        nums[5]= 989;
+        nums[6]= 84;
+        nums[7]= 65;
+        
+        
+        for(int i=0;i<=8;i++){
+            System.out.println(nums[i]);
+            
+        }
+
+
+
+    }
+}
