3062784c36e41270a27a425d8a269ac8843ae7d3
nallamaduguvk <145623934+nallamaduguvk@users.noreply.github.com>
2024-02-05 23:52:00 -0600

    while loop

---
+++ loop1.java
@@ -0,0 +1,19 @@
+public class loop1 {
+    public static void main(String[] args) {
+       // while loop
+       int i = 1;
+       while (i<=8) {
+        System.out.println("hi"+i);
+        int j = 1;
+        while (j<=3) {
+            System.out.println("hello"+ j);
+            j++;
+            
+        }
+        i++;
+        
+        
+       }
+       System.out.println("bye"+ i);
+    }
+}
