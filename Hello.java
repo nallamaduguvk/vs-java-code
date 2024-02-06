64fd6e07aa9ae5f6b90ffad8b7deb026c3966894
nallamaduguvk <145623934+nallamaduguvk@users.noreply.github.com>
2024-02-05 23:11:48 -0600

    first commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello
+{
+    public static void main(String[] args)
+    {
+        byte b = 124;
+        int a = b; 
+        System.out.println(a);
+    }
+}
---
+++ even.java
@@ -0,0 +1,11 @@
+public class even
+{
+    public static void main(String[] args)
+    {
+        int num = 28;
+        if(num % 2 == 0)
+        System.out.println("given num is even");
+        else
+        System.out.println("given num is odd");
+    }
+}
